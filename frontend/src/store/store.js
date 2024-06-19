15741cab277bc70b34dd79f9243cd50476981eea
Brayan Acosta Vivas <bacostav@ufpso.edu.co>
2024-06-19 11:56:06 -0500

    frontend

---
+++ frontend/src/store/store.js
@@ -0,0 +1,32 @@
+import { createStore } from 'vuex'
+
+const state = {
+  logged: false,
+  token: null
+}
+
+const mutations = {
+  changeLogged(state) {
+    state.logged = !state.logged
+  },
+
+  changeToken(state, newToken) {
+    state.token = newToken
+  }
+}
+
+const actions = {
+  logged: ({ commit }) => commit('changeLogged'),
+  token: ({ commit, token }) => commit('changeLogged', token)
+}
+const getters = {
+  isLoggedIn: (state) => state.logged,
+  getToken: (state) => state.token
+}
+
+export default createStore({
+  state,
+  getters,
+  actions,
+  mutations
+})
